7750ae78a3dff08896e1785292f0bf83880271c2
unknown <elimila.rodriguez@gmail.com>
2025-04-25 12:11:07 +0200

    entrega ejercicio

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ const civilHelicopters = [
     'assets/helicoptero-civil-5.jpg'
 ];
 
-const militaryHelicopter = [
+const militaryHelicopters = [
     'assets/helicoptero-militar-1.jpg',
     'assets/helicoptero-militar-2.jpg',
     'assets/helicoptero-militar-3.jpg',
@@ -32,35 +32,65 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        const randomIndex =  Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[randomIndex];
     }
     
     getRandomMilitary() {
+        const randomIndex = Math.floor(Math.random() * this.militaryImages.length);
+        return this.militaryImages[randomIndex]
     }
     
     getAll() {
+        const allImages = [...this.civilImages, ...this.militaryImages];
+        return allImages; 
     }
 }
 
 class Painter {
     constructor() {
+        this.createGallery();
+      
     }
 
     createGallery() {
+        const section = document.createElement("section");
+        document.body.appendChild(section);
+        this.gallery = section; 
     }
 
     createImageTag(imageUrl) {
+        const pictureTag = document.createElement("picture");
+        const img = document.createElement("img");
+        img.setAttribute("src",imageUrl);
+        pictureTag.appendChild(img);
+        return pictureTag;  
     }
 
+
     paintSingleImage(imageUrl) {
+        const imagenElement = this.createImageTag(imageUrl);
+        this.gallery.appendChild(imagenElement);
+
     }
 
     paintMultipleImages(arrayOfImages) {
+        arrayOfImages.forEach((imageUrl) => {
+            const tag = this.createImageTag(imageUrl);
+            this.gallery.appendChild(tag)
+
+        });
+
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
-const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
+const helicopters = new Gallery(civilHelicopters, militaryHelicopters);
 const painter = new Painter();
+
+
